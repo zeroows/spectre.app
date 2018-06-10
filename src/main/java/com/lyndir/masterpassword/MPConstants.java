8689c9e99733809ee8f1194f035d8927510c9b7f
Maarten Billemont <lhunath@lyndir.com>
2018-06-10 15:22:37 -0400

    JDK 8 support for Android platform.

---
+++ src/main/java/com/lyndir/masterpassword/MPConstants.java
@@ -0,0 +1,27 @@
+//==============================================================================
+// This file is part of Master Password.
+// Copyright (c) 2011-2017, Maarten Billemont.
+//
+// Master Password is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Master Password is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You can find a copy of the GNU General Public License in the
+// LICENSE file.  Alternatively, see <http://www.gnu.org/licenses/>.
+//==============================================================================
+
+package com.lyndir.masterpassword;
+
+/**
+ * @author lhunath, 2018-06-10
+ */
+public class MPConstants {
+
+    public static final int MS_PER_S = 1000;
+}
